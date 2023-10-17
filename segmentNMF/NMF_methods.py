fafa90b3857c33be408603f57261c2a9416533fc
GFleishman <greg.nli10me@gmail.com>
2023-10-17 09:46:13 -0400

    prefer hard 0 out of neighborhood, prefer bounding spatial values, leads to more interpretable components and time series

--- segmentNMF/NMF_methods.py
+++ segmentNMF/NMF_methods.py
@@ -210,7 +210,8 @@ def nmf(V, S_init, H_init, B, H_true=None, num_iterations=100, update_int=10, H_
         S_gradient *= S_step_size
         S[:, :n_components] += S_gradient
         S = np.maximum(0, S)
-        S[np.logical_not(B)] = 1e-12
+        S = np.minimum(1, S)  # XXX what is the significance of a spatial component voxel greater than 1?
+        S[np.logical_not(B)] = 0
 
         # Save gradient steps
         S_gradients.append(np.mean(S_gradient))
